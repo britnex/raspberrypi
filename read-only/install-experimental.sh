5d253c62b449f9347a5de2c21eacf714a5d2598f
britnex <britnex@users.noreply.github.com>
2017-10-07 15:53:34 +0200

    Update install-experimental.sh

--- read-only/install-experimental.sh
+++ read-only/install-experimental.sh
@@ -34,6 +34,9 @@ sed -i '1 s/^/overlay=yes /' /boot/cmdline.txt
 wget -nv https://raw.githubusercontent.com/britnex/raspberrypi/master/read-only/hooks_overlay -O /etc/initramfs-tools/hooks/overlay
 wget -nv https://raw.githubusercontent.com/britnex/raspberrypi/master/read-only/init-bottom_overlay -O /etc/initramfs-tools/scripts/init-bottom/overlay
 
+chmod +x /etc/initramfs-tools/hooks/overlay
+chmod +x /etc/initramfs-tools/scripts/init-bottom/overlay
+
 mkinitramfs -o /boot/initramfs.gz
 echo "initramfs initramfs.gz followkernel" >>/boot/config.txt
 
