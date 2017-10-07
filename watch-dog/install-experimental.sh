c1c9e2cf49fe5118f23d3395f53d0b4df047b6a4
britnex <britnex@users.noreply.github.com>
2017-10-07 18:16:00 +0200

    Update install-experimental.sh

--- watch-dog/install-experimental.sh
+++ watch-dog/install-experimental.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]; then
+    echo -e "ERROR: This script should be run as root" 1>&2
+    exit 1
+fi
+
+KERNEL=$(uname -r)
+
+VERSION=$(echo $KERNEL | cut -d. -f1)
+PATCHLEVEL=$(echo $KERNEL | cut -d. -f2)
+SUBLEVEL=$(echo $KERNEL | cut -d. -f3 | cut -d- -f1)
+
+KERNELVER=$(($VERSION*100000+1000*$PATCHLEVEL+$SUBLEVEL));
+
+if [ $KERNELVER -le 409040 ]; then 
+ echo "WARNING: kernel version not supported. >4.9.40 required" 1>&2
+ exit 0
+fi
+
+
+clear
+WELCOME="This script activates watchdog functionality\n
+continue installation?"
+
+if (whiptail --title "Watchdog Installation Script" --yesno "$WELCOME" 20 60) then
+    echo ""
+else
+    exit 0
+fi
+
 echo "dtparam=watchdog=on" >>/boot/config.txt
 
 sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=14/g' /etc/systemd/system.conf
